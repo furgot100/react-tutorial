0dbddd407aaedd88387d856d108da6e3a25048cc
Francis Tsang <frtsang40@gmail.com>
2021-11-11 09:56:18 -0800

    header component finshed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+// NEW
+import Title from './Title';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title />
+      <img src={logo} />
     </div>
   );
 }
 
 export default App;
 
-// github changes
+
---
+++ src/Title.js
@@ -0,0 +1,15 @@
+// src/Title.js
+
+import React from 'react'
+import './Title.css';
+
+function Title() {
+  return (
+    <div>
+      <h1>Tutorial</h1>
+      <div className="Title-Subtitle">San Franciscos Privately Owned Public Spaces</div>
+    </div>
+  )
+}
+
+export default Title
