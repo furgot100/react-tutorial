9f3f38b5214a1d37befa68b5d3578c90d918069b
Francis Tsang <frtsang40@gmail.com>
2021-11-15 15:10:30 -0800

    updated space styling

--- src/components/POPOSSpace.js
+++ src/components/POPOSSpace.js
@@ -9,15 +9,18 @@ function POPOSSpace(props) {
     return (
       <div className="POPOSSpace">
         <h1>
-            <Link to={`/details/${id}`}>
+            <Link className="POPOSSpace-title" to={`/details/${id}`}>
                 {name}
             </Link>
         </h1>
         <Link to={`/details/${id}`}>
             <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
         </Link>
-        <div>{address}</div>
-        <div>{hours}</div>
+        <div className="POPOSSpace-info">
+          <div>{address}</div>
+          <div>{hours}</div>
+        </div>
+        
       </div>
     )
   }
