cb60b29b001b18a98270475bcbdbd07756870140
Francis Tsang <frtsang40@gmail.com>
2021-11-16 09:58:58 -0800

    finished with random button and feature emojis

--- src/components/POPOSDetails.js
+++ src/components/POPOSDetails.js
@@ -3,7 +3,8 @@
 import React from 'react'
 import { useParams } from 'react-router'
 import data from '../sfpopos-data.json'
-
+import './POPOSDetails.css'
+import POPOSFeatureList from './POPOSFeatureList'
 
 
 function POPOSDetails(props) {
@@ -12,17 +13,17 @@ function POPOSDetails(props) {
   const { images, title, desc, hours, features, geo } = data[id]
 
   return (
-    <div>
-      <div>
+    <div className="POPOSDetails">
+      <div className="POPOSDetails-image">
         <img src={`${process.env.PUBLIC_URL}images/${images[0]}`} alt={title} />
       </div>
 
-      <div>
-        <h1>{ title }</h1>
-        <p>{ desc }</p>
-        <p>{ hours }</p>
-        <p>{ features }</p>
-        <p>{ geo.lat } { geo.lon }</p>
+      <div className="POPOSDetails-info">
+        <h1 className="POPOSDetails-title">{ title }</h1>
+        <p className="POPOSDetails-desc">{ desc }</p>
+        <p className="POPOSDetails-hours">{ hours }</p>
+        <POPOSFeatureList features={features}/>
+        <p className="POPOSDetails-geo">{ geo.lat } { geo.lon }</p>
       </div>
 
     </div>
--- src/components/Title.js
+++ src/components/Title.js
@@ -3,6 +3,7 @@
 import React from 'react'
 import './Title.css';
 import { NavLink } from 'react-router-dom';
+import RandomSpace from './RandomSpace/RandomSpace'
 
 function Title() {
     return (
@@ -18,6 +19,7 @@ function Title() {
             <NavLink 
                 className={({ isActive }) => isActive ? "nav-link-active" : "nav-link" }
                 to="/about">About</NavLink>
+            <RandomSpace />
             </div>
     
             </header>
